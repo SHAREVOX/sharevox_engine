cd492e17b74d349bc3781ffed7ff1c63b17eadfc
Yuto Ashida <y-chan@y-chan.dev>
2022-09-25 22:46:45 +0900

    make speaker info and metas overwritable

--- voicevox_engine/utility/copy_model_and_info.py
+++ voicevox_engine/utility/copy_model_and_info.py
@@ -34,6 +34,16 @@ def copy_model_and_info(root_dir: Path):
             if value is None:
                 installed_libraries[uuid] = True
                 shutil.copytree(root_model_dir / uuid, model_dir / uuid)
+            else:
+                # モデルの更新はしないがmetasの更新はあり得るので確認する
+                root_metas_path = root_model_dir / uuid / "metas.json"
+                installed_metas_path = model_dir / uuid / "metas.json"
+                with open(root_metas_path, encoding="utf-8") as f:
+                    root_metas = f.read()
+                with open(installed_metas_path, encoding="utf-8") as f:
+                    installed_metas = f.read()
+                if root_metas != installed_metas:
+                    shutil.copy2(root_metas_path, installed_metas_path)
         with open(libraries_json_path, "w") as f:
             json.dump(installed_libraries, f)
 
@@ -44,32 +54,64 @@ def copy_model_and_info(root_dir: Path):
         # 話者情報ディレクトリが存在する場合、話者情報の追加があるか確認する
         speaker_infos = glob.glob(str(root_speaker_info_dir / "**"))
         for uuid in [os.path.basename(info) for info in speaker_infos]:
+            root_speaker_dir = root_speaker_info_dir / uuid
             speaker_dir = speaker_info_dir / uuid
             if not speaker_dir.is_dir():
-                shutil.copytree(root_speaker_info_dir / uuid, speaker_dir)
+                shutil.copytree(root_speaker_dir, speaker_dir)
             else:
-                icons = glob.glob(str(root_speaker_info_dir / uuid / "icons" / "*"))
+                root_portrait_path = root_speaker_dir / "portrait.png"
+                portrait_path = speaker_dir / "portrait.png"
+                with open(root_portrait_path, "rb") as f:
+                    root_portrait = f.read()
+                with open(portrait_path, "rb") as f:
+                    portrait = f.read()
+                if root_portrait != portrait:
+                    shutil.copy2(root_portrait_path, portrait_path)
+
+                root_policy_path = root_speaker_dir / "policy.md"
+                policy_path = speaker_dir / "policy.md"
+                with open(root_policy_path, encoding="utf-8") as f:
+                    root_policy = f.read()
+                with open(policy_path, encoding="utf-8") as f:
+                    policy = f.read()
+                if root_policy != policy:
+                    shutil.copy2(root_policy_path, policy_path)
+
+                icons = glob.glob(str(root_speaker_dir / "icons" / "*"))
                 for icon_name in [os.path.basename(icon) for icon in icons]:
+                    root_icon_path = root_speaker_dir / "icons" / icon_name
                     icon_path = speaker_dir / "icons" / icon_name
-                    if not icon_path.is_file():
-                        shutil.copy2(
-                            root_speaker_info_dir / uuid / "icons" / icon_name,
-                            icon_path,
-                        )
+                    copy_flag = not icon_path.is_file()
+                    if not copy_flag:
+                        # 内容に更新があればcopyする
+                        with open(root_icon_path, "rb") as f:
+                            root_icon = f.read()
+                        with open(icon_path, "rb") as f:
+                            installed_icon = f.read()
+                        copy_flag = root_icon != installed_icon
+                        print("icon", copy_flag)
+                    if copy_flag:
+                        shutil.copy2(root_icon_path, icon_path)
                 voice_samples = glob.glob(
                     str(root_speaker_info_dir / uuid / "voice_samples" / "*")
                 )
                 for voice_sample_name in [
                     os.path.basename(voice_sample) for voice_sample in voice_samples
                 ]:
+                    root_voice_sample_path = (
+                        root_speaker_dir / "voice_samples" / voice_sample_name
+                    )
                     voice_sample_path = (
                         speaker_dir / "voice_samples" / voice_sample_name
                     )
-                    if not voice_sample_path.is_file():
-                        shutil.copy2(
-                            root_speaker_info_dir
-                            / uuid
-                            / "voice_samples"
-                            / voice_sample_name,
-                            voice_sample_path,
-                        )
+                    copy_flag = not voice_sample_path.is_file()
+                    if not copy_flag:
+                        # 内容に更新があればcopyする
+                        with open(root_voice_sample_path, "rb") as f:
+                            root_voice_sample = f.read()
+                        with open(voice_sample_path, "rb") as f:
+                            installed_voice_sample = f.read()
+                        copy_flag = root_voice_sample != installed_voice_sample
+                        print("vs", copy_flag)
+                    if copy_flag:
+                        shutil.copy2(root_voice_sample_path, voice_sample_path)
